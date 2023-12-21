5e5997e3f84ac973ab82c9ef051d8919e2000e86
Marilia Garcia <garciaamarilia@gmail.com>
2023-12-21 16:51:47 +0100

    feat: primeFactors function

---
+++ functions.js
@@ -0,0 +1,51 @@
+function chooseFunction(selectedFunction, inputValue) {
+ switch (selectedFunction) {
+  case "verifyPrime":
+   return primeNumber(inputValue);
+  case "primeFactors":
+   return primeFactors(inputValue);
+  default:
+   return "No Function";
+ }
+}
+
+function primeNumber(number) {
+ let divisor = 2;
+ while (number > divisor) {
+  if (number % divisor === 0) {
+   return `${number} não é primo`;
+  } else {
+   divisor++;
+  }
+ }
+ return `${number} é primo`;
+}
+
+function primeFactors(number) {
+ let factors = [],
+  divisor = 2;
+
+ while (number > 2) {
+  if (number % divisor == 0) {
+   factors.push(divisor);
+   number = number / divisor;
+  } else {
+   divisor++;
+  }
+ }
+ return `Os fatores primos são ${String(factors).split(", ")}`;
+}
+
+// HTML
+
+document
+ .getElementById("performOperationBtn")
+ .addEventListener("click", function () {
+  const selectedFunction = document.getElementById("functionSelect").value;
+  const inputValue = parseInt(document.getElementById("inputNumber").value);
+
+  const result = chooseFunction(selectedFunction, inputValue);
+
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = result;
+ });
