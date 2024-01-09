82be9f2c6aa33712f307d2649099f8f4e1054c6f
Marilia Garcia <garciaamarilia@gmail.com>
2024-01-09 16:40:44 +0100

    feat: greatest divisor function

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-function chooseFunction(selectedFunction, inputValue) {
+function chooseFunction(selectedFunction, inputValue, secondInputValue) {
  switch (selectedFunction) {
   case "verifyPrime":
    return primeNumber(inputValue);
@@ -6,6 +6,8 @@ function chooseFunction(selectedFunction, inputValue) {
    return primeFactors(inputValue);
   case "nthFibonacci":
    return fibonacciSequency(inputValue);
+  case "gcd":
+   return greatestCommonDivisor(inputValue, secondInputValue);
   default:
    return "No Function";
  }
@@ -45,15 +47,56 @@ function fibonacciSequency(number) {
   return fibonacciSequency(number - 1) + fibonacciSequency(number - 2);
  }
 }
+
+function greatestCommonDivisor(a, b) {
+ let divisor = 2,
+  greatestDivisor = 1;
+
+ if (a < 2 || b < 2) {
+  return 1;
+ }
+
+ while (a >= divisor && b >= divisor) {
+  if (a % divisor === 0 && b % divisor === 0) {
+   greatestDivisor = divisor;
+  }
+  divisor++;
+ }
+
+ return greatestDivisor;
+}
 // HTML
+
+function getFunctions(selectedFunction) {
+ const inputsDiv = document.getElementById("containerInputs");
+
+ if (selectedFunction === "gcd") {
+  inputsDiv.innerHTML = `
+    <label for="inputNumber">Enter with a number:</label>
+    <input type="text" class="form-control" id="inputNumber" placeholder="Enter with a number">
+    <label for="inputNumber2">Enter with a second number:</label>
+    <input type="text" class="form-control" id="inputNumber2" placeholder="Enter with a second number">
+    `;
+ }
+}
+
+document
+ .getElementById("functionSelect")
+ .addEventListener("change", function () {
+  const selectedFunction = document.getElementById("functionSelect").value;
+  getFunctions(selectedFunction);
+ });
 
 document
  .getElementById("performOperationBtn")
  .addEventListener("click", function () {
   const selectedFunction = document.getElementById("functionSelect").value;
   const inputValue = parseInt(document.getElementById("inputNumber").value);
+  const secondInputValue = parseInt(
+   document.getElementById("inputNumber2").value
+  );
 
-  const result = chooseFunction(selectedFunction, inputValue);
+  const result = chooseFunction(selectedFunction, inputValue, secondInputValue);
 
   const resultDiv = document.getElementById("result");
   resultDiv.innerHTML = result;
